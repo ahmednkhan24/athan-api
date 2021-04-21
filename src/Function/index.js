20aba5978d552a4182dbef156d16e351e678bbd7
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-04-20 23:07:30 -0500

    saving

---
+++ src/Function/index.js
@@ -0,0 +1,9 @@
+exports.handler = async (event, context) => {
+  // Log the event argument for debugging and for use in local development.
+  console.log(JSON.stringify(event, undefined, 2));
+
+  return {
+    statusCode: 200,
+    body: "hello world",
+  };
+};
