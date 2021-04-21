b674bcfbb806c6c6dcc05eefdb32b5af38ba73dc
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-04-21 18:13:55 -0500

    added auth and no auth

--- src/AuthFunction/index.js
+++ src/AuthFunction/index.js
@@ -4,6 +4,6 @@ exports.handler = async (event, context) => {
 
   return {
     statusCode: 200,
-    body: "hello world",
+    body: `hello world from the auth lambda. Random number: ${Math.random()}`,
   };
 };
