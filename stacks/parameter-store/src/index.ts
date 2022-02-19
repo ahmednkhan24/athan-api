8a6ddae00119588597fa313c696269ca598f706b
ahmednkhan24 <ahmednkhan24@gmail.com>
2022-02-19 14:50:49 -0600

    init parameter-store

--- stacks/parameter-store/src/index.ts
+++ stacks/parameter-store/src/index.ts
@@ -1,5 +1,11 @@
-const myFunc = () => {
-  console.log('hello world');
+// TODO figure out why fs import causes error in VSCode but not compilation
+/// <reference path="../node_modules/@types/node/fs.d.ts" />
+
+import fs from 'fs';
+
+const main = () => {
+  console.log('hello');
+  fs.writeFileSync('template.yaml', 'hi!');
 };
 
-myFunc();
+main();
