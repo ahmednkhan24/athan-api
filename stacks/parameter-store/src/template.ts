639aa3fb27ec62fc6d4512e7b5f24f849514d0a5
ahmednkhan24 <ahmednkhan24@gmail.com>
2022-02-19 20:17:30 -0600

    saving

--- stacks/parameter-store/src/template.ts
+++ stacks/parameter-store/src/template.ts
@@ -19,31 +19,11 @@ const createParameter = (param: Parameter, awsEnv: string) => `\
       Description: ${param.description}\n\
 `;
 
-export const createData = (awsEnv: string) => {
-  console.log('awsEnv: ', awsEnv);
-  const param = createParameter(parameters[0], awsEnv);
-  return `${header}${param}`;
-};
-
-// const createTemplateFileData = () => {
-//   const header = `\
-// AWSTemplateFormatVersion: 2010-09-09\n\
-// Transform: AWS::Serverless-2016-10-31\n\n\
-// Resources:\n\
-// `;
-
-//   const resources = `\
-//   MySsm:\n\
-//     Type: AWS::SSM::Parameter\n\
-//     Properties:\n\
-//       Type: String\n\
-//       Name: /my/cool/value\n\
-//       Value: hello world\n\
-//       Description: My cool SSM value\n\
-// `;
-
-//   return `${header}${resources}`;
-// };
+export const createData = (awsEnv: string) =>
+  parameters.reduce(
+    (allParams, param) => `${allParams}${createParameter(param, awsEnv)}`,
+    header
+  );
 
 export const createFile = (templateFilesData: string) => {
   writeFileSync('template.yaml', templateFilesData);
