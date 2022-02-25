6fe69bbe701bdaa9f2c9b5103bcc05907467b9bf
ahmednkhan24 <ahmednkhan24@gmail.com>
2022-02-25 14:13:53 -0600

    remove scripts

--- oldscripts/.env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo
-echo Exporting environment variables into the current shell...
-echo
-
-export AWS_PROFILE=default
-export STACKERY_STRATEGY=local
-export STACKERY_ENV_NAME=ahmedkhan-development
