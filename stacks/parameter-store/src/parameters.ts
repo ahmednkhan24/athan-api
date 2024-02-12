a577679a80d14a05a8bdf9bc947db594058761f5
ahmednkhan24 <ahmednkhan24@gmail.com>
2024-02-11 18:35:24 -0600

    tracker database parameter name

--- stacks/parameter-store/src/parameters.ts
+++ stacks/parameter-store/src/parameters.ts
@@ -13,5 +13,12 @@ export const parameters: Parameter[] = [
     description: 'Athan Alexa Skill ID',
     dev: 'amzn1.ask.skill.d637ecd3-7534-4d17-a681-31eb557020a5',
     prod: 'amzn1.ask.skill.b2707d76-213a-4a54-9905-84fad81eb7be'
+  },
+  {
+    logicalId: 'AthanTrackerDatabaseName',
+    name: 'athan-tracker-database-name',
+    description: 'Dynamo DB name for the athan tracker database',
+    dev: 'athan-tracker-database-dev',
+    prod: 'athan-tracker-database-prod'
   }
 ];
