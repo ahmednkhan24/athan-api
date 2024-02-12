ddc2d7544fd440f4d921742b95f7292d9f89a46f
ahmednkhan24 <ahmednkhan24@gmail.com>
2024-02-11 18:17:40 -0600

    remove alexa skill

--- stacks/alexa-skill/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  roots: ['<rootDir>/src/'],
-  testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
-  transform: {
-    '^.+\\.(ts)$': 'ts-jest'
-  }
-};
--- stacks/alexa-skill/src/athan/api/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const athan = axios.create({
-  baseURL: 'https://api.aladhan.com/v1'
-});
-
-export default athan;
